3c81230f903c6bfde1e7b627a9b0fcf99f64058d
jparep <parepson@gmail.com>
2025-02-06 20:44:49 -0700

    Createed configuration

--- config/config.py
+++ config/config.py
@@ -3,3 +3,14 @@ import from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
+
+# AWS PostgreSQL database configuration
+DB_CONFIG = {
+    "DB_TYPE": "postgresql",
+    "DB_DRIVER": "psycopg2",
+    "HOST": os.getenv("AWS_DB_HOST"),
+    "DATABASE": os.getenv("AWS_DB_NAME"),
+    "USER": os.getenv("AWS_DB_USER"),
+    "PASSWORD": os.getenv("AWS_DB_PASSWORD"),
+    "PORT": os.getenv("AWS_DB_PORT"),
+}
